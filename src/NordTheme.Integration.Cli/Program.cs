507a1a257f53a85f31dac1ca201e61a5f86fdfec
Justin Wendlandt <jwendl@hotmail.com>
2021-04-27 11:17:24 -0700

    Adding better integration tests

---
+++ src/NordTheme.Integration.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NordTheme.Integration.Cli
+{
+    /// <summary>
+    ///  This isn't real code. It's just so we can look at a working project and identify what works in the theme and what doesn't
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
