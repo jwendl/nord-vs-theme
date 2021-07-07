9adcf80408a41e877fccdc935aada79250a76faf
Justin Wendlandt <jwendl@hotmail.com>
2021-07-06 17:11:12 -0700

    Adding support for Visual Studio 2019

--- src/NordTheme.VisualStudio2019/Properties/AssemblyInfo.cs
+++ src/NordTheme.VisualStudio2019/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NordTheme")]
+[assembly: AssemblyTitle("NordTheme.VisualStudio2019")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NordTheme")]
+[assembly: AssemblyProduct("NordTheme.VisualStudio2019")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -27,5 +28,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.5")]
-[assembly: AssemblyFileVersion("0.0.5")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
