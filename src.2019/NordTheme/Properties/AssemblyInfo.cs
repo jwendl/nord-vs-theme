4ee4148e8969d1fd65a821ee4abd74403ce12413
Justin Wendlandt <jwendl@hotmail.com>
2021-07-06 23:41:35 -0700

    Updating files

--- src.2019/NordTheme/Properties/AssemblyInfo.cs
+++ src.2019/NordTheme/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NordTheme.VisualStudio2019")]
+[assembly: AssemblyTitle("NordTheme")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NordTheme.VisualStudio2019")]
+[assembly: AssemblyProduct("NordTheme")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,6 +27,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.5")]
 [assembly: AssemblyFileVersion("0.0.5")]
